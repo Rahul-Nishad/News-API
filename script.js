5ea8a1b04aa55a9839e4ce9a8a639b597b330066
Rahul-Nishad <123461911+Rahul-Nishad@users.noreply.github.com>
2023-09-16 17:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "3a561e252187445fa0d3bd1e5119cada";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
